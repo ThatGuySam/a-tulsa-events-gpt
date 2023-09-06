dcfef1134723b960ef02e35b5a5e1cce5c525653
ThatGuySam <sam@sam.lc>
2023-09-05 19:48:52 -0500

    Add Visit Tulsa RSS

--- tasks/fetch-sources.ts
+++ tasks/fetch-sources.ts
@@ -1,6 +1,7 @@
 import {DOMParser, parseHTML} from 'linkedom'
 
 const eventbriteTulsaUrl = 'https://www.eventbrite.com/d/ok--tulsa/all/'
+const visitTulsaRSSUrl = 'https://www.visittulsa.com/event/rss/'
 
 
 async function getLdJSON( url: string ) {
@@ -17,10 +18,30 @@ async function getLdJSON( url: string ) {
     })
 }
 
-;(async () => {
-    
+async function getRSS ( url: string ) {
+    const rawXml = await fetch( url ).then( res => res.text() )
 
+    const document = new DOMParser().parseFromString( rawXml, 'text/xml' )
+
+    console.log({ document })
+
+    // const items = document.querySelectorAll( 'item' )
+
+    // return Array.from( items ).map( ( item ) => {
+    //     const title = item.querySelector( 'title' )?.textContent || ''
+    //     const link = item.querySelector( 'link' )?.textContent || ''
+
+    //     return {
+    //         title,
+    //         link
+    //     }
+    // })
+}
+
+;(async () => {
     const ldJson = await getLdJSON( eventbriteTulsaUrl )
+
+    await getRSS( visitTulsaRSSUrl )
 
     console.log({ ldJson })
 
